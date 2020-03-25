78041a40f1b2e2e8d703cb6a78737b40a9dc1046
seokhomoon <mseokho7733@gmail.com>
2020-03-25 22:10:47 +0900

    first commit

---
+++ app/src/main/java/org/techtown/sample/MainActivity.java
@@ -0,0 +1,15 @@
+package org.techtown.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // main(String[] args])
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
