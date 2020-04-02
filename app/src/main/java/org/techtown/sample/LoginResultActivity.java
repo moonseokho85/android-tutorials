2973e2894925f2f4427b7ad21f717fb25a6dc472
seokhomoon <mseokho7733@gmail.com>
2020-04-03 00:04:46 +0900

    transfer values between Activity

---
+++ app/src/main/java/org/techtown/sample/LoginResultActivity.java
@@ -0,0 +1,34 @@
+package org.techtown.sample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginResultActivity extends AppCompatActivity {
+
+    TextView TextView_get;
+
+    // main(String[] args])
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView_get = findViewById(R.id.TextView_get);
+
+        Intent intent = getIntent();
+
+        Bundle bundle = intent.getExtras();
+        String email = bundle.getString("eamil");
+        String password = bundle.getString("password");
+
+        TextView_get.setText(email + " / " + password);
+
+    }
+}
--- app/src/main/java/org/techtown/sample/MainActivity.java
+++ app/src/main/java/org/techtown/sample/MainActivity.java
@@ -2,14 +2,44 @@ package org.techtown.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText TextInputEditText_email, TextInputEditText_password;
+    RelativeLayout RelativeLayout_login;
 
     // main(String[] args])
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInputEditText_email = findViewById(R.id.TextInputEditText_email);
+        TextInputEditText_password = findViewById(R.id.TextInputEditText_password);
+        RelativeLayout_login = findViewById(R.id.RelativeLayout_login);
+
+        // 1. 값을 가져온다.
+        // 2. 클릭을 감지한다.
+        // 3. 1번의 값을 다음 액티비티로 넘긴다.
+
+        RelativeLayout_login.setClickable(true);
+        RelativeLayout_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = TextInputEditText_email.getText().toString();
+                String password = TextInputEditText_password.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, LoginResultActivity.class);
+                intent.putExtra("email", email);
+                intent.putExtra("password", password);
+                startActivity(intent);
+            }
+        });
     }
 }
